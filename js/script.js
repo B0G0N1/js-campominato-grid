92cab91b5de26ff82624ebea4e62052eefaf293f
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-09 01:33:57 +0200

    [FEAT] Start create change difficulty option

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const playButton = document.getElementById('play-button');
 const grid = document.getElementById('grid');
+const difficulty = document.getElementById('game-difficulty').value;
 
 playButton.addEventListener('click', function() {
     grid.innerHTML = '';
     createSquare();
+    selectDifficulty();
 });
 
 function createSquare() {
@@ -12,5 +14,19 @@ function createSquare() {
         square.classList.add('square');
         square.innerText = [i];
         grid.appendChild(square);
+    }
+}
+
+function selectDifficulty() {
+    switch(difficulty) {
+        case 1:
+            alert("Difficolatà 1");
+            break;
+        case 2:
+            alert("Difficolatà 2");
+            break;
+        case 3:
+            alert("Difficolatà 3");
+            break;
     }
 }
