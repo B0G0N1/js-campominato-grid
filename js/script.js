03a165a50f5a13504d8c843b8ba25b18dade9de9
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-09 01:22:48 +0200

    [FEAT] Update and FIX createSquare Function

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 const playButton = document.getElementById('play-button');
-
 const grid = document.getElementById('grid');
 
 playButton.addEventListener('click', function() {
+    grid.innerHTML = '';
     createSquare();
 });
-
 
 function createSquare() {
     for(let i = 1; i <= 100; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
-        grid.innerText("Ciao");
+        square.innerText = [i];
         grid.appendChild(square);
     }
 }
