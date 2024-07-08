fcbe0b27d59fe17598b50493faf43f846c904571
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-09 01:50:51 +0200

    [FEAT] Update selectDifficulty function

--- js/script.js
+++ js/script.js
@@ -1,32 +1,46 @@
 const playButton = document.getElementById('play-button');
 const grid = document.getElementById('grid');
-const difficulty = document.getElementById('game-difficulty').value;
+let squareNumber;
+let squareDimension;
 
 playButton.addEventListener('click', function() {
     grid.innerHTML = '';
-    createSquare();
     selectDifficulty();
+    createSquare();
 });
 
 function createSquare() {
-    for(let i = 1; i <= 100; i++) {
+    for(let i = 1; i <= squareNumber; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
-        square.innerText = [i];
+        square.innerText = i;
+        square.style.width = squareDimension;
+        square.style.height = squareDimension;
         grid.appendChild(square);
     }
 }
 
 function selectDifficulty() {
-    switch(difficulty) {
+    const difficulty = document.getElementById('game-difficulty').value;
+    switch(parseInt(difficulty)) {
         case 1:
-            alert("Difficolatà 1");
+            alert("Difficoltà 1");
+            squareNumber = 100;
+            squareDimension = calc(1200 / 10);
             break;
         case 2:
-            alert("Difficolatà 2");
+            alert("Difficoltà 2");
+            squareNumber = 81;
+            squareDimension = calc(1200 / 8);
             break;
         case 3:
-            alert("Difficolatà 3");
+            alert("Difficoltà 3");
+            squareNumber = 49;
+            squareDimension = calc(1200 / 7);
             break;
+        default:
+            alert("Seleziona una difficoltà valida");
+            return false;
     }
+    return true;
 }
