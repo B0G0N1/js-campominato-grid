6cac7adb377c6a3fe42a2c77d82b44ab080b257e
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-09 01:16:03 +0200

    [FEAT] CreateSquare Function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 const playButton = document.getElementById('play-button');
 
+const grid = document.getElementById('grid');
+
 playButton.addEventListener('click', function() {
-    alert('Bottone cliccato!');
+    createSquare();
 });
+
+
+function createSquare() {
+    for(let i = 1; i <= 100; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        grid.innerText("Ciao");
+        grid.appendChild(square);
+    }
+}
